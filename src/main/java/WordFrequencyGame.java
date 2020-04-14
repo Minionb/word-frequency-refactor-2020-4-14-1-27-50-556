5fc28dff026ceb9e378d3e597fe4821a3a1d5847
HILARY NG (INFRA-ISD-OOCLL/HKG) <NGHI2@oocl.com>
2020-04-14 15:32:21 +0800

    Hilary: refract the script format

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -2,12 +2,11 @@ import java.util.*;
 
 public class WordFrequencyGame {
     private static final String SPACE_PATTERN = "\\s+";
-    public static final String DELIMITER = "\n";
-    public static final String CALCULATE_ERROR = "Calculate Error";
-    public static final String SPACE_DELIMITER = " ";
+    private static final String DELIMITER = "\n";
+    private static final String CALCULATE_ERROR = "Calculate Error";
+    private static final String SPACE_DELIMITER = " ";
 
     public String getResult(String sentence) {
-
         try {
            List<WordInfo> wordInfoList = calculateWordFrequency(sentence);
            return formatWordFrequencyResult(wordInfoList);
@@ -15,24 +14,24 @@ public class WordFrequencyGame {
         } catch (Exception e) {
             return CALCULATE_ERROR;
         }
-
     }
 
     private List<WordInfo> calculateWordFrequency(String sentence) {
         List<String> words = Arrays.asList(sentence.split(SPACE_PATTERN));
         List<WordInfo> wordInfoList = new ArrayList<>();
+
         for(String word : new HashSet<>(words)) {
             int count = Collections.frequency(words, word);
             wordInfoList.add(new WordInfo(word, count));
-
         }
+
         wordInfoList.sort((firstWordInfo, secondWordInfo) -> secondWordInfo.getWordCount() - firstWordInfo.getWordCount());
         return wordInfoList;
-
     }
 
     private String formatWordFrequencyResult(List<WordInfo> wordInfoList) {
         StringJoiner joiner = new StringJoiner(DELIMITER);
+
         for (WordInfo wordInfo : wordInfoList) {
             String wordWithCount = wordInfo.getWord() + SPACE_DELIMITER + wordInfo.getWordCount();
             joiner.add(wordWithCount);
